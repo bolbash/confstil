4ffc0caecb45827218590913d7c4a1a3888d90a2
bolbash <155198595+bolbash@users.noreply.github.com>
2024-03-13 03:38:56 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,10 +4,10 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = [
+let userID = 
 	'1ddbeb13-0ec6-4a02-ad2b-b28ea8308b59',
 	'76eab2c7-ce4d-47dc-a0b6-7151dfa08eb0'
-];
+;
 let proxyIP = "49.51.46.56";
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
